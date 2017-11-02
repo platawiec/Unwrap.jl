a284199b15bae6f5a5c7b673ac08a91a5a3ab9e5
platawiec <platawiec@hotmail.com>
2017-11-02 14:48:20 -0400

    1D unwrap implemented

--- src/unwrap_1d.jl
+++ src/unwrap_1d.jl
@@ -1,1 +1,16 @@
-function unwrap!()
+function unwrap!(A::AbstractVector)
+    previous_element = A[1]
+    difference = zero(previous_element)
+    periods = 0
+    for i in 2:length(A)
+        difference = A[i] - previous_element
+        if difference > π
+            periods -= 1
+        elseif difference < -π
+            periods += 1
+        end
+        previous_element = A[i]
+        A[i] = previous_element + 2 * π * periods
+    end
+    return A
+end

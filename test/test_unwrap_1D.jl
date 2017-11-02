ab93da8e7bdf1bc1c378385a7ad0106c6c53fd3c
platawiec <platawiec@hotmail.com>
2017-11-02 14:13:39 -0400

    Add in-place test

--- test/test_unwrap_1D.jl
+++ test/test_unwrap_1D.jl
@@ -2,3 +2,7 @@ A_unwrapped = collect(linspace(0, 10π, 100))
 @. A_wrapped = A_unwrapped % (2π)
 
 @assert unwrap(A_wrapped) == A_unwrapped
+
+# test in-place version
+unwrap!(A_wrapped)
+@assert A_wrapped == A_unwrapped

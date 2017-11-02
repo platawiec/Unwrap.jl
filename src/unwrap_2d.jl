178c6c6747041727b58b88ae1bd2fb373429c3ac
platawiec <platawiec@hotmail.com>
2017-11-02 18:51:54 -0400

    Minor changes

--- src/unwrap_2d.jl
+++ src/unwrap_2d.jl
@@ -8,7 +8,7 @@ end
 mutable struct Pixel{T}
     periods::Int64
     num_pixels_in_group::Int64
-    value::T
+    val::T
     reliability::Float64
     id_group::Int64
     id_newgroup::Int64
@@ -21,17 +21,18 @@ mutable struct Edge{T}
     pixel_2::Pixel{T}
     periods::Int64
 end
+Base.isless(e1::Edge, e2::Edge) = isless(e1.reliability, e2.reliability)
 
 function unwrap!(wrapped_image::AbstractMatrix)
 
-    mod = convert(typeof(a), 2π)
+    mod = convert(eltype(wrapped_image), 2π)
     params = UnwrapParameters(mod, false, false, 0)
     pixel_image = initialise_pixels(wrapped_image)
     calculate_reliability(pixel_image, params)
     edges_h = calculate_reliability_horizontal_edges(pixel_image, params)
     edges_v = calculate_reliability_vertical_edges(pixel_image, params)
 
-    sort_edges()
+    sort(edges)
 
     gather_pixels()
 
@@ -53,7 +54,7 @@ function calculate_reliability(pixel_image, params)
     const top_pixel = CartesianIndex(1, 0)
     const bot_pixel = CartesianIndex(-1, 0)
     # inner loop
-    for i in CartesianRange(CartesianIndex(2,2), CartesianIndex(size(A)[1]-1, size(A)[2]-1))
+    for i in CartesianRange(CartesianIndex(2,2), CartesianIndex(size(pixel_image)[1]-1, size(pixel_image)[2]-1))
         H = wrap(pixel_image[i+left_pixel].val - pixel_image[i].val)
         V = wrap(pixel_image[i+right_pixel].val - pixel_image[i].val)
         D1 = wrap(pixel_image[i+top_pixel].val - pixel_image[i].val)
@@ -68,12 +69,13 @@ function calculate_reliability_horizontal_edges(pixel_image, params)
     edges_horizontal = Array{Edge{typeof(pixel_image[1].val)}}(e_size)
 
     for i in CartesianRange(size(edges_horizontal))
-        edges_horizontal.pixel_1 = pixel_image[i]
-        edges_horizontal.pixel_2 = pixel_image[i+CartesianIndex(1, 0)]
-        edges_horizontal.reliability = (pixel_image[i].reliability
-                                        + pixel_image[i+CartesianIndex(1, 0)].reliability)
-        edges_horizontal.periods = find_period(pixel_image[i].val,
+        edges_horizontal[i] = Edge((pixel_image[i].reliability
+                                        + pixel_image[i+CartesianIndex(1, 0)].reliability),
+                                    pixel_image[i],
+                                    pixel_image[i+CartesianIndex(1,0)],
+                                    find_period(pixel_image[i].val,
                                                pixel_image[i+CartesianIndex(1, 0)].val)
+                                   )
         params.num_edges += 1
     end
 
@@ -85,12 +87,13 @@ function calculate_reliability_vertical_edges(pixel_image, params)
     edges_vertical = Array{Edge{typeof(pixel_image[1].val)}}(e_size)
 
     for i in CartesianRange(size(edges_vertical))
-        edges_vertical.pixel_1 = pixel_image[i]
-        edges_vertical.pixel_2 = pixel_image[i+CartesianIndex(0, 1)]
-        edges_vertical.reliability = (pixel_image[i].reliability
-                                      + pixel_image[i+CartesianIndex(0, 1)].reliability)
-        edges_vertical.periods = find_period(pixel_image[i].val,
-                                             pixel_image[i+CartesianIndex(0, 1)].val)
+        edges_vertical[i] = Edge((pixel_image[i].reliability
+                                        + pixel_image[i+CartesianIndex(0, 1)].reliability),
+                                    pixel_image[i],
+                                    pixel_image[i+CartesianIndex(0, 1)],
+                                    find_period(pixel_image[i].val,
+                                                pixel_image[i+CartesianIndex(0, 1)].val)
+                                   )
         params.num_edges += 1
     end
 
@@ -103,6 +106,8 @@ function gather_pixels(edges, params)
             # pixel 2 is alone in group
             if edge.pixel_2.num_pixels_in_group == 1
                 merge_pixels!(edge.pixel_1, edge.pixel_2)
+            end
+        end
     end
 end
 
@@ -117,6 +122,7 @@ function wrap(val)
         wrapped_val = val - 2π
     elseif val < -π
         wrapped_val = val + 2π
+    end
     return wrapped_val
 end
 

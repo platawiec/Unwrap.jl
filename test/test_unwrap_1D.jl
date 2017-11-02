fcf4ae60df91b63cf491bbf7cb76004eb9d0569d
platawiec <platawiec@hotmail.com>
2017-11-02 14:38:05 -0400

    Syntax fix

--- test/test_unwrap_1D.jl
+++ test/test_unwrap_1D.jl
@@ -1,8 +1,8 @@
 A_unwrapped = collect(linspace(0, 10π, 100))
-@. A_wrapped = A_unwrapped % (2π)
+A_wrapped = A_unwrapped .% (2π)
 
-@assert unwrap(A_wrapped) == A_unwrapped
+@test unwrap(A_wrapped) == A_unwrapped
 
 # test in-place version
 unwrap!(A_wrapped)
-@assert A_wrapped == A_unwrapped
+@test A_wrapped == A_unwrapped

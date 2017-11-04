3e8452130c829ff0dd311b62ed58bfec396eff21
platawiec <platawiec@hotmail.com>
2017-11-04 11:57:51 -0400

    Fix tests

--- test/test_unwrap_2D.jl
+++ test/test_unwrap_2D.jl
@@ -1,9 +1,12 @@
-vec_unwrapped = linspace(0, 5π, 10)
+vec_unwrapped = linspace(0, 5π, 20)
 mat_unwrapped = vec_unwrapped .+ vec_unwrapped'
 mat_wrapped = mat_unwrapped .% (2π)
 
-@test unwrap(mat_wrapped) == mat_unwrapped
+uw_test = unwrap(mat_wrapped)
+difference = mat_unwrapped[1,1] - uw_test[1,1]
+@test (uw_test + difference) ≈ mat_unwrapped
 
 # test in-place version
 unwrap!(mat_wrapped)
-@test mat_wrapped == mat_unwrapped
+difference = mat_unwrapped[1,1] - mat_wrapped[1,1]
+@test (mat_wrapped + difference) ≈ mat_unwrapped

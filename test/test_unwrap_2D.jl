097a5417e01218a92d47cdee79eff2b19e00cb7c
platawiec <platawiec@hotmail.com>
2017-11-04 12:24:05 -0400

    Added BigFloat tests

--- test/test_unwrap_2D.jl
+++ test/test_unwrap_2D.jl
@@ -10,3 +10,12 @@ difference = mat_unwrapped[1,1] - uw_test[1,1]
 unwrap!(mat_wrapped)
 difference = mat_unwrapped[1,1] - mat_wrapped[1,1]
 @test (mat_wrapped + difference) ≈ mat_unwrapped
+
+vec_bf = linspace(0, 5*BigFloat(π), 20)
+mat_bf_uw = vec_bf .+ vec_bf'
+mat_bf_w = mat_bf_uw .% 2*BigFloat(π)
+
+uw_bf_test = unwrap(mat_bf_w)
+difference = mat_bf_uw[1,1] - uw_bf_test[1,1]
+@test eltype(uw_bf_test) == BigFloat
+@test (uw_bf_test + difference) ≈ mat_bf_uw

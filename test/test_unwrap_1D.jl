406532e7b54d64ad307478015f764a45f9963d91
Michael Pusterhofer <michael.pusterhofer@ams.com>
2019-05-08 16:59:15 +0200

    should fix 1d test in 1.0

--- test/test_unwrap_1D.jl
+++ test/test_unwrap_1D.jl
@@ -7,10 +7,10 @@ A_wrapped = A_unwrapped .% (2π)
 unwrap!(A_wrapped)
 @test A_wrapped == A_unwrapped
 
-A_bf_uw = collect(range(0, 10*BigFloat(π), length=100))
+A_bf_uw = collect(range(0, stop=10*BigFloat(π), length=100))
 A_bf_w = A_bf_uw .% (2*BigFloat(π))
 @test unwrap(A_bf_w) ≈ A_bf_uw
 
-A_f32_uw = collect(range(0, 10*Float32(π), length=100))
+A_f32_uw = collect(range(0, stop=10*Float32(π), length=100))
 A_f32_w = A_bf_uw .% (2*Float32(π))
 @test unwrap(A_bf_w) ≈ A_bf_uw
